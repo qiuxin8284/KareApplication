da25d0a3fc8d9a9ee044a61e9d98242281ab70f6
qiuxin8284 <lili8284>
2019-06-23 20:43:36 +0800

    Merge branch 'master' of D:\KDR\corn_andriod_app_qx\KareApplication with conflicts.

--- app/src/main/java/com/kaer/more/activity/MainActivity.java
+++ app/src/main/java/com/kaer/more/activity/MainActivity.java
@@ -1,14 +1,26 @@
 package com.kaer.more.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.kaer.more.R;
+import com.tencent.liteav.demo.play.SuperPlayerModel;
+import com.tencent.liteav.demo.play.SuperPlayerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SuperPlayerView mSuperPlayerView;
+    private String videoUrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +32,57 @@ public class MainActivity extends AppCompatActivity {
 //      SYSTEM_UI_FLAG_FULLSCREEN表示全屏的意思，也就是会将状态栏隐藏
         //设置系统UI元素的可见性
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+
+        //hideBottomUIMenu();//隐藏虚拟按键，否则全屏时虚拟按键会影响视频播放进度条的调整。
+        checkPermission();//检查权限
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        initView();
+
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        videoUrl = "http://www.jmzsjy.com/UploadFile/微课/地方风味小吃——宫廷香酥牛肉饼.mp4";
+        mSuperPlayerView = (SuperPlayerView) findViewById(R.id.video_player_item);
+        playVideo();
+    }
+
+    /**
+     * 检查权限
+     */
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            List<String> permissions = new ArrayList<>();
+            if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)) {
+                permissions.add(Manifest.permission.CAMERA);
+            }
+            if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+            if (permissions.size() != 0) {
+                ActivityCompat.requestPermissions(this, permissions.toArray(new String[0]), 100);
+            }
+        }
+    }
+
+    /**
+     * 播放视频
+     */
+    private void playVideo() {
+        SuperPlayerModel model = new SuperPlayerModel();
+        model.videoURL = "http://200024424.vod.myqcloud.com/200024424_709ae516bdf811e6ad39991f76a4df69.f20.mp4";
+        mSuperPlayerView.playWithMode(model);
+    }
+    private void stopVideo() {
+        //mSuperPlayerView.
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSuperPlayerView.resetPlayer();
     }
 }
