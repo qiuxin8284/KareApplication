baf2fbdfc80cf46ea12848bef36b13b8cf2db589
qiuxin8284 <lili8284>
2019-06-24 22:33:07 +0800

    做好模拟广告播放

--- app/src/main/java/com/kaer/more/activity/MainActivity.java
+++ app/src/main/java/com/kaer/more/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.kaer.more.activity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,8 +12,11 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.kaer.more.R;
+import com.kaer.more.entitiy.AdvertisementData;
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.tencent.liteav.demo.play.SuperPlayerModel;
 import com.tencent.liteav.demo.play.SuperPlayerView;
 
@@ -20,7 +25,48 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private SuperPlayerView mSuperPlayerView;
-    private String videoUrl;
+    private ImageView mIvTextPic;
+    private TextView mTvText;
+    private ArrayList<AdvertisementData> list = new ArrayList<AdvertisementData>();
+    private int nowPosition = 0;
+    private static final int GO_AD = 0;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case GO_AD:
+                    if (list.size() != 0) {
+                        if (nowPosition == list.size()) nowPosition = 0;
+                        AdvertisementData advertisementData = list.get(nowPosition);
+                        //播放广告
+                        if (advertisementData.getMediaType() == 1) {//文本
+                            mTvText.setVisibility(View.VISIBLE);
+                            mIvTextPic.setVisibility(View.VISIBLE);
+                            mSuperPlayerView.setVisibility(View.GONE);
+                            mSuperPlayerView.onPause();
+                            mTvText.setText(advertisementData.getContent());
+                            ImageLoader.getInstance().displayImage(advertisementData.getMedia(), mIvTextPic);
+                        } else if (advertisementData.getMediaType() == 2) {//图片
+                            mTvText.setVisibility(View.GONE);
+                            mIvTextPic.setVisibility(View.VISIBLE);
+                            mSuperPlayerView.setVisibility(View.GONE);
+                            mSuperPlayerView.onPause();
+                            ImageLoader.getInstance().displayImage(advertisementData.getMedia(), mIvTextPic);
+                        } else if (advertisementData.getMediaType() == 3) {//广告
+                            mTvText.setVisibility(View.GONE);
+                            mIvTextPic.setVisibility(View.GONE);
+                            mSuperPlayerView.setVisibility(View.VISIBLE);
+                            playVideo(advertisementData.getMedia());
+                        }
+                        nowPosition = nowPosition + 1;
+                        mHandler.sendEmptyMessageDelayed(GO_AD, advertisementData.getDuration() * 1000);
+                    }
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,15 +85,48 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         initView();
 
+        initList();
+    }
+
+    private void initList() {
+        //获取网络数据
+        //先模拟数据
+        list = new ArrayList<AdvertisementData>();
+        AdvertisementData advertisementData = new AdvertisementData();//模拟文本
+        advertisementData.setMediaType(1);
+        advertisementData.setContent("广告测试文本1");
+        advertisementData.setMedia("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561396474168&di=f209408b5f8bcbfdcd27dec8ff9a7c9a&imgtype=0&src=http%3A%2F%2Fpic25.nipic.com%2F20121112%2F9252150_150552938000_2.jpg");
+        advertisementData.setDuration(10);
+        list.add(advertisementData);
+        advertisementData = new AdvertisementData();//模拟视频
+        advertisementData.setMediaType(3);
+        advertisementData.setDuration(30);
+        advertisementData.setMedia("http://www.jmzsjy.com/UploadFile/微课/地方风味小吃——宫廷香酥牛肉饼.mp4");
+        list.add(advertisementData);
+        advertisementData = new AdvertisementData();//模拟图片
+        advertisementData.setMediaType(2);
+        advertisementData.setDuration(10);
+        advertisementData.setMedia("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561396439119&di=b52f8459a8c209324c638503b4c9005c&imgtype=0&src=http%3A%2F%2Fimg.redocn.com%2Fsheji%2F20141219%2Fzhongguofengdaodeliyizhanbanzhijing_3744115.jpg");
+        list.add(advertisementData);
+        advertisementData = new AdvertisementData();//模拟视频
+        advertisementData.setMediaType(3);
+        advertisementData.setDuration(30);
+        advertisementData.setMedia("http://200024424.vod.myqcloud.com/200024424_709ae516bdf811e6ad39991f76a4df69.f20.mp4");
+        list.add(advertisementData);
+        //开始执行第一条
+        nowPosition = 0;
+        mHandler.sendEmptyMessage(GO_AD);
+
     }
 
     /**
      * 初始化控件
      */
     private void initView() {
-        videoUrl = "http://www.jmzsjy.com/UploadFile/微课/地方风味小吃——宫廷香酥牛肉饼.mp4";
+        //String videoUrl = "http://www.jmzsjy.com/UploadFile/微课/地方风味小吃——宫廷香酥牛肉饼.mp4";
         mSuperPlayerView = (SuperPlayerView) findViewById(R.id.video_player_item);
-        playVideo();
+        mIvTextPic = (ImageView) findViewById(R.id.iv_text_and_pic);
+        mTvText = (TextView) findViewById(R.id.tv_text);
     }
 
     /**
@@ -71,11 +150,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      * 播放视频
      */
-    private void playVideo() {
+    private void playVideo(String url) {
+        //url = "http://200024424.vod.myqcloud.com/200024424_709ae516bdf811e6ad39991f76a4df69.f20.mp4";
         SuperPlayerModel model = new SuperPlayerModel();
-        model.videoURL = "http://200024424.vod.myqcloud.com/200024424_709ae516bdf811e6ad39991f76a4df69.f20.mp4";
+        model.videoURL = url;
         mSuperPlayerView.playWithMode(model);
     }
+
     private void stopVideo() {
         //mSuperPlayerView.
     }
---
+++ app/src/main/java/com/kaer/more/entitiy/AdvertisementData.java
@@ -0,0 +1,110 @@
+package com.kaer.more.entitiy;
+
+public class AdvertisementData {
+    private int mediaType;//1文本，2图片，3视频
+    private int adType;//1商业广告，2公益广告
+    private int duration;//时长
+    private int liveCount;//播放次数
+    private String liveTime;//播放时间
+    private String location;//定位
+    private String limits;//范围（单位km)
+    private String content;//广告内容
+    private String media;//媒体文件
+    private int level;//优先级
+
+    public int getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(int mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public int getAdType() {
+        return adType;
+    }
+
+    public void setAdType(int adType) {
+        this.adType = adType;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getLiveCount() {
+        return liveCount;
+    }
+
+    public void setLiveCount(int liveCount) {
+        this.liveCount = liveCount;
+    }
+
+    public String getLiveTime() {
+        return liveTime;
+    }
+
+    public void setLiveTime(String liveTime) {
+        this.liveTime = liveTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLimits() {
+        return limits;
+    }
+
+    public void setLimits(String limits) {
+        this.limits = limits;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getMedia() {
+        return media;
+    }
+
+    public void setMedia(String media) {
+        this.media = media;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "AdvertisementData{" +
+                "mediaType=" + mediaType +
+                ", adType=" + adType +
+                ", duration=" + duration +
+                ", liveCount=" + liveCount +
+                ", liveTime='" + liveTime + '\'' +
+                ", location='" + location + '\'' +
+                ", limits='" + limits + '\'' +
+                ", content='" + content + '\'' +
+                ", media='" + media + '\'' +
+                ", level=" + level +
+                '}';
+    }
+}
